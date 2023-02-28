d0dd5b340733f28d25c39944360b06a675d3012d
vinicius-virgilli <vinicius.virgilli3@gmail.com>
2023-02-28 07:24:55 -0300

    exercício note converter feito

--- 1 - Fundamentos/3 - Introdução à JavaScript/Dia 01: JavaScript - Primeiros passos/paraFixar1.js
+++
@@ -1,5 +0,0 @@
-let myName = 'Vinicius';
-let birthCity = 'Goiânia';
-let birthYear = 1995;
-
-console.log(myName,'\n',birthCity,'\n',birthYear);
--- 1 - Fundamentos/3 - Introdução à JavaScript/Dia 01: JavaScript - Primeiros passos/paraFixar6.js
+++
@@ -1,7 +0,0 @@
-let weekday = 'Domingo';
-
-if (weekday == 'segunda-feira' || 'terça-feira' || "quarta-feira" || 'quinta-feira' || 'sexta-feira') {
-    console.log('Oba, mais um dia de aprendizado na Trybe >:D');
-} else if (weekday = 'Sábado' || 'Domingo') {
-    console.log('FINALMENTE, descanso merecido');
-}
