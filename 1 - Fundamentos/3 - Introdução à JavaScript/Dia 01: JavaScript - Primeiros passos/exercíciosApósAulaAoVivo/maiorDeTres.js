5b61eec1190f7fff0f257de0f01e2dc851d483ac
vinicius-virgilli <vinicius.virgilli3@gmail.com>
2023-02-27 21:16:54 -0300

    adiciona os 3 primeiros exercícios feitos e o arquivo do quarto exercício

---
+++ 1 - Fundamentos/3 - Introdução à JavaScript/Dia 01: JavaScript - Primeiros passos/exercíciosApósAulaAoVivo/maiorDeTres.js
@@ -0,0 +1,11 @@
+let a = 55;
+let b = 11;
+let c = 89;
+
+if (a > b && a > c) {
+    console.log(a + ' é o maior dos três ');
+} else if (b > a && b > c) {
+    console.log(b + ' é o maior dos três ');
+} else {
+    console.log(c + ' é o maior dos três ');
+}
