6b65c09e4e63c143db66ee2f7394a3eece06b204
vinicius-virgilli <vinicius.virgilli3@gmail.com>
2023-02-28 15:47:33 -0300

    feito exercícios para fixar 7 e 8

---
+++ 1 - Fundamentos/3 - Introdução à JavaScript/Dia 02: JavaScript - Array e loop For/exercícios Para Fixar/paraFixar7.js
@@ -0,0 +1,28 @@
+// Algoritmo para se arrumar para uma festa
+
+1º - a festa é de gala (chique)? Se sim preciso alugar a roupa pois não a tenho no momento. Caso Não, siga as instruções abaixo:
+2º - tome banho
+3º - escove os dentes
+4º - passe o creme de cabelo no cabelo, e penteie o cabelo
+vá - até o quarto, pegue uma cueca e vista a cueca
+5º - vá até o quarto, pegue uma calça passada e vista a calça
+6º - pegue um par de sapatos na sapateira e calce os sapatos
+7º - peque uma camisa bonita e passada e vista a camisa
+8º - pegue o celular ponha no bolso da calça
+
+perguntas:
+
+- Eu resolvi o problema?
+R: Sim, depois destes passos estarei pronto para ir à festa
+
+- Havia outras maneiras de resolver o problema?
+R: Com certeza, tem infinitas possibilidades
+
+- A maneira que eu escolhi foi a mais eficiente ou havia uma forma de resolver o problema com menos passos?
+R: Acredito que escolhi uma das maneiras mais eficientes, mão provavelmente existe uma com menos passos
+
+- Seria possível inverter ou retirar algum passo?
+R: Sim, pode vestir a camiseta primeiro e depois vestir a calça
+
+- Se eu fosse um computador, conseguiria entender todas as instruções?
+Não, se eu fosse um computador teria de haver muito mais instruções
---
+++ 1 - Fundamentos/3 - Introdução à JavaScript/Dia 02: JavaScript - Array e loop For/exercícios Para Fixar/paraFixar8.js
@@ -0,0 +1,29 @@
+let fruits = [3, 4, 10, 1, 12];
+let sum = 0;
+
+for (number of fruits) {
+    sum += number
+}
+
+if (sum > 15) {
+    console.log(sum);
+} else {
+    console.log('Valor menor que 16');
+}
+
+Perguntas:
+
+- Eu resolvi o problema?
+R: Sim
+
+- Havia outras maneiras de resolver o problema?
+Sim, colocando variáveis com outros nomes ou utilizando o for ao invés do for of, por exemplo
+
+- A maneira que eu escolhi foi a mais eficiente possível?
+R: Acredito que sim
+
+- Seria possível inverter ou retirar algum passo?
+R: Sim, poderia declarar a variável sum dentro do loop, por exemplo
+
+- Se eu fosse um computador, conseguiria entender todas as instruções?
+Sim. Inclusive meu computador entendeu rs!
