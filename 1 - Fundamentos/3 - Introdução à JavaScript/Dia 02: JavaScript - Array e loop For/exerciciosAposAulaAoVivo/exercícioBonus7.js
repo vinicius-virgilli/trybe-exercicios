f65007df1315e4d9f9f655d47ef099a7140fb92d
vinicius-virgilli <vinicius.virgilli3@gmail.com>
2023-03-01 09:04:32 -0300

    exercício bônus 7 feitooooo

--- 1 - Fundamentos/3 - Introdução à JavaScript/Dia 02: JavaScript - Array e loop For/exerciciosAposAulaAoVivo/exercícioBonus7.js
+++ 1 - Fundamentos/3 - Introdução à JavaScript/Dia 02: JavaScript - Array e loop For/exerciciosAposAulaAoVivo/exercícioBonus7.js
@@ -1,8 +1,8 @@
-let n = 7
+let n = 25
 let pyramid = '';
 let countOdd = 0;
-let lineSize = Math.trunc(n /2) + 1;
-let firstCharacterPosition = Math.trunc(n / 2);
+let lineSize = Math.trunc(n /2);
+let firstCharacterPosition = Math.trunc(n / 2 + 1);
 // Como o nº de linhas da pirâmide é igual ao número de ímpares de 1 até n,
 //vamos descobrir quantos ímpares temos
 
@@ -14,8 +14,8 @@ for (let index = 1; index <= n; index += 1) {
 
 for (let index = 0; index < countOdd; index += 1) {
     pyramid = '';
-    lineSize += index;
-    firstCharacterPosition += - index;
+    lineSize += 1;
+    firstCharacterPosition += -1;
 
     for (let secondIndex = 0; secondIndex < lineSize; secondIndex += 1) {
         if (secondIndex < firstCharacterPosition) {
