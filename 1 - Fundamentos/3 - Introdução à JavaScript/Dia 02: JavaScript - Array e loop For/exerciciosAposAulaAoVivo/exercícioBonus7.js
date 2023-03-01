9f21138a8fd34f3a5b9ea80866ca857c22874787
vinicius-virgilli <vinicius.virgilli3@gmail.com>
2023-03-01 07:32:39 -0300

    fazendo o exercício bônus 7

--- 1 - Fundamentos/3 - Introdução à JavaScript/Dia 02: JavaScript - Array e loop For/exerciciosAposAulaAoVivo/exercícioBonus7.js
+++ 1 - Fundamentos/3 - Introdução à JavaScript/Dia 02: JavaScript - Array e loop For/exerciciosAposAulaAoVivo/exercícioBonus7.js
@@ -1,3 +1,30 @@
 let n = 5
 let pyramid = '';
+let countOdd = 0;
+let lineSize = Math.trunc(n /2) + 1;
+let firstCharacterPosition = Math.trunc(n / 2);
+// Como o nº de linhas da pirâmide é igual ao número de ímpares de 1 até n,
+//vamos descobrir quantos ímpares temos
+
+for (let index = 1; index <= n; index += 1) {
+    if (index % 2 !== 0) {
+        countOdd += 1;
+    }
+}
+
+for (let index = 0; index < countOdd; index += 1) {
+    lineSize += index;
+    firstCharacterPosition += - index;
+
+    for (let secondIndex = 0; secondIndex <= lineSize; secondIndex += 1) {
+        if (secondIndex < firstCharacterPosition) {
+            pyramid += ' ';
+        } else {
+            pyramid += '*';
+        }
+    }
+    console.log(pyramid);
+}
+
+
 
