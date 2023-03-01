6ecaecffd87ead4dd4328ce9e0406ac9c63b605b
vinicius-virgilli <vinicius.virgilli3@gmail.com>
2023-02-28 21:15:28 -0300

    exercício lógica de programação e algoritmos, feito

--- 1 - Fundamentos/3 - Introdução à JavaScript/Dia 02: JavaScript - Array e loop For/exerciciosAposAulaAoVivo/logicaDeProgramacaoEAlgoritmos.js
+++ 1 - Fundamentos/3 - Introdução à JavaScript/Dia 02: JavaScript - Array e loop For/exerciciosAposAulaAoVivo/logicaDeProgramacaoEAlgoritmos.js
@@ -1,6 +1,6 @@
 let tenFactorial = 1;
 let initialString = 'amor';
-let reverseString = 'kkkk';
+let reverseString = '';
 
 for (let index = 10; index > 0; index -= 1) {
     tenFactorial = tenFactorial * index
@@ -10,8 +10,28 @@ console.log('10! = ' + tenFactorial);
 
 for (let index = 0; index < initialString.length; index += 1) {
     let reverseStringIndex = (initialString.length -1 - index); 
-    reverseString = initialString[reverseStringIndex];
+    reverseString += initialString[reverseStringIndex];
 }
 
 console.log('\nO inverso de ' + initialString + ' é ' + reverseString);
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let biggerWord = array[0];
+let smallerWord = array[0];
+
+for (let index = 1; index < array.length; index += 1) {
+    if (array[index].length > biggerWord.length) {
+        biggerWord = array[index]
+    }
+
+    if (array[index].length < smallerWord.length) {
+        smallerWord = array[index];
+    }
+}
+
+console.log('\nA maior string no array é: ' + biggerWord);
+console.log('A menor string no array é: ' + smallerWord);
+
+
+
 
