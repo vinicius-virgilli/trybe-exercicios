79df3927dfae84c5d6c002cee09adf7a1bf89f16
vinicius-virgilli <vinicius.virgilli3@gmail.com>
2023-03-02 10:31:41 -0300

    exercícios para fixar 2 feito

---
+++ 1 - Fundamentos/3 - Introdução à JavaScript/Dia 04 - JavaScript ES6 - let, const, arrow functions e/exercícios para fixar/paraFixar3.js
@@ -0,0 +1,7 @@
+// Usando o objeto abaixo, utilize for/in e imprima um console.log com as chaves e valores desse objeto.
+
+let car = {
+    model: 'A3 Sedan',
+    manufacturer: 'Audi',
+    year: 2020
+  };
