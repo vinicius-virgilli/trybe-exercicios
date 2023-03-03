88a05768441e8e6779f8a187cd3d6d8d07e915fe
vinicius-virgilli <vinicius.virgilli3@gmail.com>
2023-03-03 11:30:05 -0300

    exercícios do dia 3.4 da 3º seção de fundamentos feitos, exceto os bônus. Vou entrar no conteúdo do dia 3.5 e fazer os bônus posteriormente

--- 1 - Fundamentos/3 - Introdução à JavaScript/Dia 04 - JavaScript ES6 - let, const, arrow functions e/exercícios para fixar/paraFixar3.js
+++
@@ -1,7 +0,0 @@
-// Usando o objeto abaixo, utilize for/in e imprima um console.log com as chaves e valores desse objeto.
-
-let car = {
-    model: 'A3 Sedan',
-    manufacturer: 'Audi',
-    year: 2020
-  };
