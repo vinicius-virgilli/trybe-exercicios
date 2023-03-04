ddbe1292278c79ad2eaa5572af2985f0ae22f7f2
vinicius-virgilli <vinicius.virgilli3@gmail.com>
2023-03-04 11:34:57 -0300

    exercício romanNumerConverter feito

---
+++ 1 - Fundamentos/3 - Introdução à JavaScript/Dia 04: JavaScript - Objetos/exercícios após aula ao vivo/exercícios bônus/exercicioBonus2.js
@@ -0,0 +1,4 @@
+/* Crie uma função chamada arrayOfNumbers, que receberá a variável vector como parâmetro.
+
+Por meio de um loop for, percorra essa variável, busque os números pares e os adicione a um novo array, que deverá ser retornado pela função no fim. */
+
